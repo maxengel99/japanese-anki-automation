b38ed1c8f569dc244ac6aa807de8e38893c4471b
Max Engel <maxengel@Maxs-MacBook-Pro.local>
2021-04-04 19:02:00 -0700

    Stardew valley text

--- anki_request.py
+++ anki_request.py
@@ -14,7 +14,7 @@ class AnkiRequest:
         audio_json = {'url': 'https://raw.githubusercontent.com/maxengel99/japanese-anki-automation/master/mp3/{}.mp3'.format(cur_word[0]), 'filename': '{}.mp3'.format(cur_word[0]),
                       'fields': ['Audio']}
         deck_name = 'Japanese Vocab'
-        model_name = 'Japanese Vocab'
+        model_name = 'Japanese Vocab-690b6'
         json_args = {'deckName': deck_name, 'modelName': model_name,
                      'fields': fields, 'options': {'allowDuplicate': False},
                      'tags': [], 'audio': audio_json}
@@ -23,6 +23,7 @@ class AnkiRequest:
 
     def invoke(self, params):
         """Makes request to add anki card"""
+        print(params)
         response = json.load(urllib.request.urlopen(
             'http://localhost:8765', json.dumps(params).encode('utf-8')))
         return response
